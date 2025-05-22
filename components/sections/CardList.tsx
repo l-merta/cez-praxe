ca8ea4eb48c248117abfca422e0c352f5055ecda
l-merta <lukas.merta26@gmail.com>
2025-05-22 10:13:12 +0200

    use delete

--- components/sections/CardList.tsx
+++ components/sections/CardList.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState, useRef, useCallback, use } from "react"
+import { useEffect, useState, useRef, useCallback } from "react"
 
 import Card from '@/components/Card'
 import { Button } from "@/components/ui/button"
